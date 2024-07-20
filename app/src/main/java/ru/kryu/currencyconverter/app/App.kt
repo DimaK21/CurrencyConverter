40e8a5ca9ed93b0023b5327161053ebbbdfd7ac5
Dmitry Kryushnikov <civ02@mail.ru>
2024-07-20 19:21:38 +0500

    App

---
+++ app/src/main/java/ru/kryu/currencyconverter/app/App.kt
@@ -0,0 +1,7 @@
+package ru.kryu.currencyconverter.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/ru/kryu/currencyconverter/app/MainActivity.kt
+++ app/src/main/java/ru/kryu/currencyconverter/app/MainActivity.kt
@@ -1,11 +1,14 @@
-package ru.kryu.currencyconverter
+package ru.kryu.currencyconverter.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
+import ru.kryu.currencyconverter.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
